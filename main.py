a95a1a95879ab3b96f1103eb202da27080cbfc56
pettdev <pettdev@gmail.com>
2022-12-13 19:00:41 -0400

    Clase Bola y Clase Limites creadas por mi

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 import pygame as pg
+from clase_bolilla import *
 
 #Inicializar todos los modulos pygame
 #pantallas, sonidos, teclados, etc
 pg.init()
 
 # Crear pantalla o Surface
-pantalla_principal = pg.display.set_mode( (800, 600) ) # Ventana y tamaño, con Ancho y Largo
+ancho_pant = 600
+alto_pant = 800
+pantalla_principal = pg.display.set_mode( (alto_pant, ancho_pant) ) # Ventana y tamaño, con Ancho y Largo
 pg.display.set_caption("Bolillas Rebotando") # Titulo para la ventana
 
 game_over = False
 
-x = 400
-y = 300
-vx = 1
-vy = 1
+velocidad = 1
 
-z = 000
-a = 000
-vz = 0.5
-va = 0.5
+coord = Limites(400, 300, 20, 20, ancho_pant, alto_pant, velocidad)
+coord1 = Limites(200, 100, 20, 20, ancho_pant, alto_pant, velocidad)
+coord2 = Limites(100, 200, 20, 20, ancho_pant, alto_pant, velocidad)
+coord3 = Limites(300, 150, 20, 20, ancho_pant, alto_pant, velocidad)
+
+bola, bola1, bola2, bola3 = Bolilla, Bolilla, Bolilla, Bolilla
+
 
 while not game_over:
 
@@ -29,24 +32,14 @@ while not game_over:
 
     pantalla_principal.fill( (52, 152, 219) ) #Asignar color a la pantalla
 
-    x += vx
-    y += vy
-    z += vz
-    a += va
-
-    if x >= 780 or x == 0:
-        vx *= -1
-    if y >= 580 or y == 0:
-        vy *= -1
-    if z >= 780 or z == 0:
-        vz *= -1
-    if a >= 580 or a == 0:
-        va *= -1
-    
-
     # Crear un rectángulo que tenga largo y ancho
+
     #dibujar(pantalla, (color RGB), (Coord. Posicion An x La, tamaño An, Tamaño La))
-    pg.draw.rect(pantalla_principal, (192, 57, 43), (x, y, 20, 20))
-    pg.draw.rect(pantalla_principal, (50, 168, 82), (z, a, 40, 40))
+    """ pg.draw.rect(pantalla_principal, (192, 57, 43), (x, y, 20, 20)) """
+
+    bola(20, 20, (192, 57, 36), pantalla_principal, coord.coordLimites()[0], coord.coordLimites()[1])
+    bola1(20, 20, (132, 13, 74), pantalla_principal, coord1.coordLimites()[0], coord1.coordLimites()[1])
+    bola2(20, 20, (111, 86, 18), pantalla_principal, coord2.coordLimites()[0], coord2.coordLimites()[1])
+    bola3(20, 20, (98, 32, 96), pantalla_principal, coord3.coordLimites()[0], coord3.coordLimites()[1])
 
     pg.display.flip() #Para aplicar todo el añadido de arriba.
